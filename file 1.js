aa0630ee178bed3097ba249a5af2e4c94740e972
Akinola <61994563+Akinolaayodeji@users.noreply.github.com>
2020-03-24 11:32:57 +0100

    Add files via upload

---
+++ file 1.js
@@ -0,0 +1,15 @@
+var myName = "Akinola Ayodeji";
+console.log(myName)
+var myCourse = ["Html-Css-Js", "Design", "Python"]
+console.log(myCourse)
+var x;
+
+for(x = 0;x<=200;x++){
+
+if(x%2 != 0){
+
+console.log(x);
+
+}
+
+}
